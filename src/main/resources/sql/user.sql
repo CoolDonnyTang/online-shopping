24675c75df69abe63363e461643ea82b98151bfb
Donny <tangyanrentyr@163.com>
2017-02-08 17:29:46 +0800

    建立user表的sql文件及DowerDesigner

---
+++ src/main/resources/sql/user.sql
@@ -0,0 +1,37 @@
+/*
+SQLyog Ultimate v11.25 (64 bit)
+MySQL - 5.6.26 : Database - online_shopping
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`online_shopping` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `online_shopping`;
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(10) unsigned zerofill NOT NULL AUTO_INCREMENT,
+  `nickname` varchar(20) NOT NULL,
+  `mobile_number` varchar(20) DEFAULT NULL,
+  `email` varchar(50) NOT NULL,
+  `id-number` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `user` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
