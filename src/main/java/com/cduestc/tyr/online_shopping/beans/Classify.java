e4185176098332b53d84a0474761686ac1ddfaa2
Donny <tangyanrentyr@163.com>
2017-03-02 23:32:54 +0800

    管理界面进行到添加主分类阶段

---
+++ src/main/java/com/cduestc/tyr/online_shopping/beans/Classify.java
@@ -0,0 +1,57 @@
+package com.cduestc.tyr.online_shopping.beans;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="classify")
+public class Classify {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	private Integer belong;
+	private Integer entryId;
+	private Long entryTime;
+	private Long lastChangeTime;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getBelong() {
+		return belong;
+	}
+	public void setBelong(Integer belong) {
+		this.belong = belong;
+	}
+	public Integer getEntryId() {
+		return entryId;
+	}
+	public void setEntryId(Integer entryId) {
+		this.entryId = entryId;
+	}
+	public Long getEntryTime() {
+		return entryTime;
+	}
+	public void setEntryTime(Long entryTime) {
+		this.entryTime = entryTime;
+	}
+	public Long getLastChangeTime() {
+		return lastChangeTime;
+	}
+	public void setLastChangeTime(Long lastChangeTime) {
+		this.lastChangeTime = lastChangeTime;
+	}
+	
+}
---
+++ src/main/webapp/resources/scripts/manage.js
@@ -0,0 +1,37 @@
+function operKindsDis(num) {
+	if(1 === num) {
+		$("#add_main").show();
+		$("#add_subsidiary").hide();
+		$("#delete_main").hide();
+		$("#delete_subsidiary").hide();
+	} else if(2 === num) {
+		$("#add_main").hide();
+		$("#add_subsidiary").show();
+		$("#delete_main").hide();
+		$("#delete_subsidiary").hide();
+		$.ajax({
+			data:"",
+			dataType:"jason",
+			error:function() {
+				
+			},
+			success:function(result) {
+				if(result.status === 1) {
+					
+				}
+			},
+			type:"post",
+			url:""
+		});
+	} else if(3 === num) {
+		$("#add_main").hide();
+		$("#add_subsidiary").hide();
+		$("#delete_main").show();
+		$("#delete_subsidiary").hide();
+	} else if(4 === num) {
+		$("#add_main").hide();
+		$("#add_subsidiary").hide();
+		$("#delete_main").hide();
+		$("#delete_subsidiary").show();
+	}
+}
